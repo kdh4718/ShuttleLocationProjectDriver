68257d99c9fc741e318377d585948d444d754a03
ThinkingDobby <chiho1221@naver.com>
2022-05-05 14:07:17 +0900

    Parcelize 코드 제거

--- app/src/main/java/kr/rabbito/shuttlelocationprojectdriver/data/Location.kt
+++ app/src/main/java/kr/rabbito/shuttlelocationprojectdriver/data/Location.kt
@@ -1,13 +1,9 @@
 package kr.rabbito.shuttlelocationprojectdriver.data
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 class Location(
     val driverName: String,
     var latitude: Double,
     var longitude: Double
-) : Parcelable {
+) {
     constructor() : this("", 0.0, 0.0)
 }
