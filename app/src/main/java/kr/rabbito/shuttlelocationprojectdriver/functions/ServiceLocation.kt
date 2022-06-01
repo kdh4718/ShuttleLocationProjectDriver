38c0d887a829fa42285f354dd6c1c1eebae8d4bf
sts06017 <87319059+sts06017@users.noreply.github.com>
2022-06-01 16:01:41 +0900

    노티 수정, 앱 종료시 서비스 종료되도록 수정

--- app/src/main/java/kr/rabbito/shuttlelocationprojectdriver/functions/ServiceLocation.kt
+++ app/src/main/java/kr/rabbito/shuttlelocationprojectdriver/functions/ServiceLocation.kt
@@ -1,6 +1,5 @@
 package kr.rabbito.shuttlelocationprojectdriver.functions
 
-import android.R
 import android.app.*
 import android.content.Context
 import android.content.Intent
@@ -9,10 +8,10 @@ import android.location.LocationManager
 import android.os.Build
 import android.os.CountDownTimer
 import android.os.IBinder
-import android.os.SystemClock.sleep
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.GlobalScope
@@ -21,18 +20,19 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kr.rabbito.shuttlelocationprojectdriver.MainActivity
 import kr.rabbito.shuttlelocationprojectdriver.data.Location
-                    /** 서비스 **/
+
+/** 서비스 **/
                     //https://developer.android.com/guide/components/services?hl=ko#kotlin
                     //https://qwerty-ojjj.tistory.com/37
 class ServiceLocation : Service() {
     private lateinit var sendJob: Job
     private lateinit var locationManager :LocationManager
     private lateinit var loc : LocationListener
-
+    private lateinit var manager:NotificationManager
     val mid= arrayOf(37.3456, 126.7392)     //운전자가 범위내에 있는지 확인을 위한 중간지점.
     var result = arrayOf(37.3395, 126.7325) //초기값을 0,0 -> 학교정류장으로 변경.
     private lateinit var timer:CountDownTimer //일정시간 이후 스레드 종료를 위한 타이머객체 변수.
-
+    lateinit var notification : Notification
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         //서비스가 호출될때?마다 실행됨. 여러번 호출가능하므로 여러개의 쓰레드가
         //실행될 가능성이 있음. 그래서 위치정보 전송은 onCreate()에서 실행됨.
@@ -45,13 +45,13 @@ class ServiceLocation : Service() {
                 "알림 설정 모드 타이틀",
                 NotificationManager.IMPORTANCE_DEFAULT)
             //val manager = getSystemService(NotificationManager::class.java)!!
-            val manager = getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
+            manager = getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
             manager.createNotificationChannel(serviceChannel)
         }
 
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
-        val notification: Notification = NotificationCompat.Builder(this, "CHANNEL_ID")
+        notification = NotificationCompat.Builder(this, "CHANNEL_ID")
             .setContentTitle("셔틀위치알리미")
             .setContentText("실시간 위치 정보를 전송 중입니다.")
             .setSmallIcon(resources.getIdentifier("location_icon", "drawable", this.packageName))
@@ -59,10 +59,9 @@ class ServiceLocation : Service() {
             .setAutoCancel(true)
             .setOngoing(false)
             .build()
-        startForeground(1, notification)
+        startForeground(100, notification)
         //여기까지 notification띄우기
         Log.d("서비스","notification띄움")
-
         //onStartCommand()는 정수반환함.
         return super.onStartCommand(intent, flags, startId)
     }
@@ -86,7 +85,7 @@ class ServiceLocation : Service() {
         Log.d("서비스","위치정보전송 시작?")
 
         //타이머 객체 시간,주기,할일 설정 -- 8시간(28800초)동안,10분 주기
-        timer = object : CountDownTimer(1000*600*6*8,1000*600){
+        timer = object : CountDownTimer(1000*600*6*8,1000*6){
             override fun onTick(p0: Long) {//주기마다 할일 메소드
                 //범위내에 있는지 체크, 벗어날 시 서비스 종료.
                 Log.d("서비스","거리를 확인합니다. 현재위치:${result[0]},${result[1]}")
@@ -112,6 +111,14 @@ class ServiceLocation : Service() {
         //location 정보 전송을 종료시킴.
         sendJob.cancel()
         timer.cancel()    //서비스 종료시 타이머도 종료시킴.
+        //manager.cancel(100)//서비스가 강제종료될때 노티를 삭제함.
+        //강제종료가 되어 서비스가 종료됨을 알리는 노티 띄움.
+        notification = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setContentTitle("셔틀위치알리미")
+            .setContentText("실시간 위치 정보 전송 종료.")
+            .setSmallIcon(resources.getIdentifier("location_icon", "drawable", this.packageName))
+            .build()
+        startForeground(100,notification)
         Toast.makeText(this,"위치 전송이 중단되었습니다.",Toast.LENGTH_SHORT).show()
     }
     private fun makeLocationSendRoutine(ref: DatabaseReference, location: Location, group: String, id: String, data: Array<Double>): Job {
