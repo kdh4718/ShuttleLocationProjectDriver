c60537afd69e7a78ded207352857cd8787775098
ThinkingDobby <chiho1221@naver.com>
2022-06-10 13:08:13 +0900

    위치 권한 요청 시점 변경

--- app/src/main/java/kr/rabbito/shuttlelocationprojectdriver/MainActivity.kt
+++ app/src/main/java/kr/rabbito/shuttlelocationprojectdriver/MainActivity.kt
@@ -1,9 +1,13 @@
 package kr.rabbito.shuttlelocationprojectdriver
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kr.rabbito.shuttlelocationprojectdriver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     private var mBinding: ActivityMainBinding? = null
     //매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
     private val binding get() = mBinding!!
+
+    private val LOCATION_REQUEST = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         overridePendingTransition(0, 0)
 
-        startLoading()
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            var permissions = arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_REQUEST)
+        } else {
+            startLoading()
+        }
     }
 
     private fun startLoading() {
@@ -30,6 +45,36 @@ class MainActivity : AppCompatActivity() {
             finish()
             startActivity(intent)
         }, 1000)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode === LOCATION_REQUEST) {
+            if (grantResults.size > 0) {
+                for (grant in grantResults) {
+                    // 권한 획득하지 못한 경우
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "앱 사용을 위해서는 위치 권한이 필요합니다. 앱을 재시작 해주세요.", Toast.LENGTH_SHORT).show()
+                    } else {    // 권한 획득한 경우
+                        if (ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED
+                        ) {
+                            startLoading()
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
